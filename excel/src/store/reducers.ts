aa4775a4d53ff197fe3578ecfabe9fd5f1e0e3aa
Alfred Lemon <alfred.lemon@mail.utoronto.ca>
2020-05-26 09:24:45 -0400

    Add reducer documentation

--- excel/src/store/reducers.ts
+++ excel/src/store/reducers.ts
@@ -3,6 +3,11 @@ import { combineReducers } from '@reduxjs/toolkit'
 import ExcelStore from './ExcelStore'
 
 const rootReducer = combineReducers({
+  /**
+   * Normally, you would get reducers using ExcelStore.reducer since reducers are normally
+   * Created using createSlice. However, the ExcelStore uses the Undox middleware,
+   * whose return type is the reducer itself
+   */
   Excel: ExcelStore,
 })
 
