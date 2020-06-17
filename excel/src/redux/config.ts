d6cea65ad6b204888f8366cccab62a3858688ca2
Alfred Lemon <alfred.lemon@mail.utoronto.ca>
2020-06-16 23:16:18 -0400

    Omit inactive sheets from redux dev tools

--- excel/src/redux/config.ts
+++ excel/src/redux/config.ts
@@ -1,13 +1,13 @@
 import IRootStore from '../@types/redux/store'
 import thunk from 'redux-thunk'
 
-export const devTools = {
+export const devTools: any = {
   stateSanitizer: (state: IRootStore) => ({
     Excel: {
       ...state.Excel,
       present: {
         ...state.Excel.present,
-        inactiveSheets: {},
+        inactiveSheets: '<<LONG_BLOB>>',
       },
     },
   }),
--- excel/src/redux/store.ts
+++ excel/src/redux/store.ts
@@ -2,13 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
 import IRootStore from '../@types/redux/store'
-import { middleware } from './config'
+import { middleware, devTools } from './config'
 
 export const useTypedSelector: TypedUseSelectorHook<IRootStore> = useSelector
 
 const store = configureStore({
   reducer: rootReducer,
-  // devTools,
+  devTools,
   middleware,
 })
 
