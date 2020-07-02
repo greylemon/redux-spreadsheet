07ff4787731ed9f1b4e66b2eaa522023c9469307
Alfred Lemon <alfred.lemon@mail.utoronto.ca>
2020-07-01 22:16:33 -0400

    Remove storybook deployment from travis

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -2,4 +2,4 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { NonRoute } from './NonRoute.stories'
 
-storiesOf('Router', module).add('No Route', NonRoute as any)
+storiesOf('Router', module).add('No Route', () => <NonRoute />)
