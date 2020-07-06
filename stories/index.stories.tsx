79403b6bd58ccda6dbe974d8b4b95a51528fd2cc
Alfred Lemon <alfred.lemon@mail.utoronto.ca>
2020-07-06 00:07:49 -0400

    Add parameters to storybook

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -2,8 +2,10 @@ import { storiesOf } from '@storybook/react'
 import { NonRoute } from './NonRoute.stories'
 import { Formulas } from './Formula.stories'
 
-storiesOf('Formulas', module).add('Formula', Formulas)
-// .addParameters({ info: { inline: true } })
+storiesOf('Formulas', module)
+  .add('Formula', Formulas)
+  .addParameters({ info: { inline: true } })
 
-storiesOf('Router', module).add('No Route', NonRoute)
-// .addParameters({ info: { inline: true } })
+storiesOf('Router', module)
+  .add('No Route', NonRoute)
+  .addParameters({ info: { inline: true } })
