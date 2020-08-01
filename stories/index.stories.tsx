d803b4099fa2f30a14ef5c40aef7614b38ef9ac9
Alfred Lemon <alfred.lemon@mail.utoronto.ca>
2020-08-01 14:13:41 -0400

    Add frozen example

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -2,9 +2,14 @@ import { storiesOf } from '@storybook/react'
 import NonRoute from './NonRoute.stories'
 import Formulas from './Formula.stories'
 import './index.scss'
+import Frozen from './Frozen.stories'
 
 storiesOf('Formulas', module)
   .add('Formula', Formulas)
+  .addParameters({ info: { inline: true } })
+
+storiesOf('Frozen Panes', module)
+  .add('Frozen', Frozen)
   .addParameters({ info: { inline: true } })
 
 storiesOf('Router', module)
